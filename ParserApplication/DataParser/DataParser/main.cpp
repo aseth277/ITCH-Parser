870fadce4e5c49a35fa0c877483e43da3f3843b6
Anshul Seth <aseth277@gmail.com>
2022-12-17 12:41:39 +0530

    first commit

---
+++ ParserApplication/DataParser/DataParser/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 5) {
+        std::cerr << "Insuffucient arguments" << std::endl;
+        return 1;
+    }
+
+    std::string pathFile = argv[1];
+    std::string outBookDirectory = argv[2];
+    std::string outMessageDirectory = argv[3];
+    size_t levels = std::stoul(argv[4]);
+    std::string stock = argv[5];
+    std::string nameFile = getFileName(pathFile);
+    std::string outBookFileName = outBookDirectory + nameFile + "_" + stock + "_book_" + std::to_string(levels) + ".csv";
+    std::string outMessageFileName = outMessageDirectory + nameFile + "_" + stock + "_message.csv";
+    std::string stockPadded = stock;
+    stockPadded.insert(stockPadded.end(), 8 - stockPadded.size(), ' ');
+
+    BookConstructor BookConstructor(pathFile,
+        outMessageFileName,
+        outBookFileName,
+        stockPadded,
+        levels);
+
+    BookConstructor.start();
+
+    return 0;
+}
